cd86c2bfca746b4e1cd9d02c6e9f348ec8c6e818
resource11 <mcmahon@resource11.com>
2023-06-18 07:04:55 -0400

    feat: adds svg icons, minor cleanup

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -2,7 +2,7 @@
 
 import classnames from "classnames"
 import { useExtraClasses } from "../../utils/useExtraClasses"
-import { Icon } from "../Icon/NotAn-index"
+// import { Icon } from "../Icon/Icon"
 import styles from "./Link.module.css"
 
 export const LinkSizes = {
