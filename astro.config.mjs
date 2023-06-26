55b425082df990af9013f091e1299655df1ff1a9
resource11 <mcmahon@resource11.com>
2023-06-26 06:59:40 +0300

    fix: adds .npmrc, cleans up astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig, sharpImageService } from "astro/config";
-// import image from "@astrojs/image";
-// import image from "@astrojs/image/sharp";
 import react from "@astrojs/react";
 import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
@@ -8,17 +6,12 @@ import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
 // import netlify from "@astrojs/netlify/edge-functions"
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     syntaxHighlight: "prism",
     gfm: true,
   },
   integrations: [
-    // image({
-    //   serviceEntryPoint: "@astrojs/image/sharp",
-    // }),
-
     mdx({
       drafts: true,
     }),
